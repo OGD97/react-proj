f0e3573d154a7a9d859a184ba876f29b819114f8
OGD97 <omardaya1997@hotmail.com>
2024-06-28 22:24:52 +0200

    sign-in form page added

--- app/index.jsx
+++ app/index.jsx
@@ -9,8 +9,9 @@ export default function App(){
     return(
         <SafeAreaView className="bg-primary h-full">
             <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center h-full px-4">
-        <Image source={images.logo}
+        <View className="w-full justify-center items-center min-h-[84vh] px-4">
+        <Image 
+        source={images.logo}
              className="w-[130px] h-[84px]"
              resizeMode='contain'
             />
@@ -36,7 +37,7 @@ export default function App(){
 
         <CustomButton 
         title="Continue with Email"
-        handlePress={()=> router.push('/home')}
+        handlePress={()=> router.push('/sign-in')}
         containerStyles="w-full mt-7"/>
 
         </View>
